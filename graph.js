818d560f8275c59dd37e6b5949520155638c2890
EdTriplett <EdTriplett@users.noreply.github.com>
2017-09-22 16:52:51 -0400

    almost populating matrix

--- graph.js
+++ graph.js
@@ -2,8 +2,8 @@ const EDGE_LIST = require("./edgeList");
 
 class AdjacencyMatrix {
   constructor(EDGE_LIST) {
-    this.matrix = new Array(this.getLength(EDGE_LIST)).fill(
-      new Array(this.getLength(EDGE_LIST))
+    this.matrix = new Array(this.getLength(EDGE_LIST)+1).fill(
+      new Array(this.getLength(EDGE_LIST)+1)
     );
   }
   getLength(array) {
@@ -17,9 +17,12 @@ class AdjacencyMatrix {
   }
 
   populateMatrix(EDGE_LIST) {
-    EDGE_LIST.map(connection => {
-      console.log(connection);
+    EDGE_LIST.forEach(connection => {
+      for (let i=1; i<=EDGE_LIST.length; i++) {
+        this.matrix[i][i] = null
+      }
       this.matrix[connection[0].id][connection[1].id] = connection[2];
+      this.matrix[connection[1].id][connection[0].id] = connection[2]
     });
   }
 }
@@ -27,3 +30,4 @@ class AdjacencyMatrix {
 const test = new AdjacencyMatrix(EDGE_LIST);
 
 test.populateMatrix(EDGE_LIST);
+console.log(test.matrix)
