43a12be51a5251ba49259483634247dc8e93c471
Eric Glover <etgt28@mail.missouri.edu>
2017-09-22 17:26:20 -0500

    statifying things

--- adjacencyList.js
+++ adjacencyList.js
@@ -13,6 +13,7 @@ class AdjacencyList {
   // printNode(node){
   //   return `${this.}`
   // }
+
   addPeopleArray() {
     this.personArray.forEach(edge => {
       if (!this.adList[edge[0].id]) {
@@ -36,6 +37,29 @@ class AdjacencyList {
       console.log(`from: ${name}\t`);
     });
   }
+  showStats() {
+    //total number of vertices and edges
+    console.log(`number of vertices ${this.adList.length}`);
+    const edges = this.adList.reduce(
+      (sum, list) => (list ? sum + list.length : sum),
+      0
+    );
+    console.log(`number of edges ${edges}`);
+    //most connected
+    const degrees = this.adList.map((list, index) => {
+      return {
+        name: this.name[index],
+        degree: list ? list.length : 0
+      };
+    });
+    let sorted = degrees.sort((a, b) => b.degree - a.degree);
+    console.log(
+      `Top three most connected ${sorted[0].name}(${sorted[0]
+        .degree}), ${sorted[1].name}(${sorted[1].degree}), ${sorted[2]
+        .name}(${sorted[2].degree})`
+    );
+    //strongest 3 connections
+  }
   findHighId(personArray) {
     let id = 0;
 
@@ -53,6 +77,7 @@ const test = () => {
   console.log("adjList = ", adjList);
   console.log("edgeWeight 2<->10 is = ", adjList.edgeWeight(2, 10));
   adjList.printAdjList();
+  adjList.showStats();
 };
 
 test();
