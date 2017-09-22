ad68bafd45b040d9615bdb1829103b8565146614
Benjamin Soung <soungbenjamin@gmail.com>
2017-09-22 16:25:30 -0400

    weight done

--- adjacencyList.js
+++ adjacencyList.js
@@ -1,5 +1,5 @@
-const EDGE_LIST = require("./_edgeList");
-const LinkedList = require("./linkedList");
+const EDGE_LIST = require('./_edgeList');
+const LinkedList = require('./linkedList');
 
 class AdjacencyMatrix {
   constructor(edgeList) {
@@ -18,12 +18,52 @@ class AdjacencyMatrix {
   display() {
     this.edges.forEach(edge => {
       if (edge.head) {
-        console.log(edge.person.name, " is connected to:");
+        console.log(edge.person.name, ' is connected to:');
         console.log(edge.toString());
       }
     });
   }
+
+  edgeWeight(left, right) {
+    let current = this.edges[left].head;
+    // console.log(this.edges[left]);
+    while (current.data.id !== right && current.next) {
+      current = current.next;
+    }
+
+    return current.data.id === right ? current.data.weight : null;
+  }
 }
 
 const adjacencyMatrix = new AdjacencyMatrix(EDGE_LIST);
-adjacencyMatrix.display();
+console.log(adjacencyMatrix.edgeWeight(1, 4));
+
+// adjacencyMatrix.display();
+
+/*
+Mike  is connected to:
+{"id":12,"name":"Garrett","weight":5}
+
+Adam  is connected to:
+{"id":17,"name":"Andur","weight":8}
+{"id":0,"name":"Bob","weight":18}
+
+Peter  is connected to:
+{"id":2,"name":"Sally","weight":5}
+{"id":11,"name":"Donald","weight":10}
+{"id":4,"name":"Michael","weight":12}
+
+Andur  is connected to:
+{"id":5,"name":"Michelle","weight":17}
+{"id":14,"name":"Mike","weight":9}
+
+Tom  is connected to:
+{"id":16,"name":"Peter","weight":14}
+{"id":14,"name":"Mike","weight":9}
+
+Boris  is connected to:
+{"id":13,"name":"Xin","weight":7}
+{"id":12,"name":"Garrett","weight":5}
+{"id":18,"name":"Tom","weight":3}
+{"id":2,"name":"Sally","weight":5}
+ */
