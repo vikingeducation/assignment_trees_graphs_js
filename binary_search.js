78b307f03bd9165b0351c6ef9f2e87486d2c5ca9
EdTriplett <EdTriplett@users.noreply.github.com>
2017-09-22 12:09:49 -0400

    shall ow add

--- binary_search.js
+++ binary_search.js
@@ -10,32 +10,32 @@ class BinaryTree {
   constructor(array) {
     this.array = array;
     this.root = new Node(this.array.shift());
-//    this.addNode = this.addNode.bind(this)
+    //    this.addNode = this.addNode.bind(this)
     while (this.array.length) {
-      this.addNode(this.array.shift())
+      this.addNode(this.array.shift());
     }
   }
 
-  addNode(value, current=this.root) {
+  addNode(value, current = this.root) {
     //left condition
     if (value < current.data) {
       if (current.left) {
-        this.addNode(value, current.left)
+        this.addNode(value, current.left);
       }
-      return current.left = new Node(value)
+      return (current.left = new Node(value));
     } else {
       //right condition
       if (current.right) {
-        this.addNode(value, current.right)
+        this.addNode(value, current.right);
       }
-      return current.right = new Node(value);
+      return (current.right = new Node(value));
     }
   }
 }
 /* 
           6
-        /   
-        4
+        /  \
+        4   9
       /
     3
   / \
@@ -53,4 +53,4 @@ console.log(binary_table.root.right);
 console.log(binary_table.root.left.left);
 console.log(binary_table.root.left.right);
 console.log(binary_table.root.left.left);
-console.log('6, 4, 9, 3, 5, 1')
+console.log("6, 4, 9, 3, 5, 1");
