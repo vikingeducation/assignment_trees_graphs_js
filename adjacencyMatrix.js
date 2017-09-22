9d461d56f3d28b19e085adf8d17004287df0d7da
Eric Glover <etgt28@mail.missouri.edu>
2017-09-22 13:00:12 -0500

    printing matrixesces

--- adjacencyMatrix.js
+++ adjacencyMatrix.js
@@ -3,6 +3,7 @@ const edgeList = require("./edgeList");
 class AdjacencyMatrix {
   constructor(personArray) {
     let highId = this.findHighId(personArray);
+    this.name = [];
     this.matrix = new Array(highId + 1)
       .fill(null)
       .map(() => new Array(highId + 1).fill(null));
@@ -24,9 +25,38 @@ class AdjacencyMatrix {
 
   insertWeights(personArray) {
     personArray.forEach(edge => {
+      this.name[edge[0].id] = edge[0].name;
+      this.name[edge[1].id] = edge[1].name;
       this.matrix[edge[0].id][edge[1].id] = edge[2];
     });
+  }
+  printMatrix() {
+    process.stdout.write("\t");
+    this.name.forEach(name => process.stdout.write(`\t${name}`));
+    console.log("");
+    this.matrix.forEach((arr, index) => {
+      process.stdout.write(`${this.name[index]}\t`);
+      arr.forEach(element => {
+        if (!element) {
+          process.stdout.write(`\tX`);
+          return;
+        }
+        process.stdout.write(`\t${element}`);
+      });
+      console.log("");
+    });
+  }
+
+  edgeWeight(from, to) {
+    return this.matrix[from][to];
   }
 }
 
 new AdjacencyMatrix(edgeList);
+
+const test = () => {
+  const matrix = new AdjacencyMatrix(edgeList);
+  console.log("edgeWieght(1,10), = ", matrix.edgeWeight(1, 10));
+  matrix.printMatrix();
+};
+test();
