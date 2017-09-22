e8677d9db9d4ef11ea8276cbfcaa8cda0f965c08
Alex-Willenbrink <awillenbrink@gmail.com>
2017-09-22 11:12:06 -0500

    printing trees

--- binaryTree.js
+++ binaryTree.js
@@ -4,43 +4,65 @@ class Node {
   constructor(data, left = null, right = null) {
     this.left = left;
     this.right = right;
-    thid.data = data;
+    this.data = data;
   }
 }
 
 class BinaryTree {
   constructor(array) {
     this.root = new Node(array[0], null, null);
-    makeTree(array.slice(1));
+    this.makeTree(array.slice(1));
   }
+
   makeTree(array) {
-    //
     array.forEach(num => {
-      // const node = new Node(num);
-      insertNode(new Node(num), this.root);
-      // insertNode(node, this.root);
+      this.insertNode(new Node(num), this.root);
     });
   }
+
   insertNode(node, parent) {
     if (parent.data > node.data) {
       if (parent.left) {
-        insertNode(node, parent.left);
+        this.insertNode(node, parent.left);
       } else {
-        //set
         parent.left = node;
       }
     } else if (parent.data < node.data) {
       if (parent.right) {
-        insertNode(node, parent.right);
+        this.insertNode(node, parent.right);
       } else {
         parent.right = node;
       }
-    } else {
-      //they're equal and do nothing
     }
+    //they're equal and do nothing
+  }
+
+  printTree(queue) {
+    if (!queue.length) return;
+    // load the data first
+    let length = queue.length;
+    for (let i = 0; i < length; i++) {
+      if (queue[i] === null) {
+        process.stdout.write(" null ");
+        continue;
+      }
+      queue.push(queue[i].left);
+      queue.push(queue[i].right);
+
+      process.stdout.write(` ${queue[i].data} `);
+    }
+    process.stdout.write("\n");
+
+    queue = queue.slice(length);
+    this.printTree(queue);
+  }
+
+  print() {
+    this.printTree([this.root]);
   }
 }
 
-const test = () => {};
+const test = new BinaryTree([8, 10, 3, 1, 6, 14, 4, 7, 13]);
+test.print();
 
 /////
