6e2b857f431573cc5231872b06d6c1680177f013
Benjamin Soung <soungbenjamin@gmail.com>
2017-09-22 12:07:28 -0400

    graphs

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -8,6 +8,11 @@ class Node {
 
 class Tree {
   constructor(iter) {
+    if (iter) {
+      iter.forEach(item => {
+        this.add(item);
+      });
+    }
     this.root;
   }
 
@@ -25,7 +30,7 @@ class Tree {
           break;
         }
       }
-      current[current.data > data ? "left" : "right"] = new Node(data);
+      current[current.data > data ? 'left' : 'right'] = new Node(data);
     }
   }
 
@@ -34,12 +39,42 @@ class Tree {
   }
 }
 
-const tree = new Tree();
+const tree = new Tree([8, 10, 3, 1, 6, 14, 4, 7, 13]);
 
-tree.add(6);
-tree.add(5);
-tree.add(4);
-tree.add(3);
-tree.add(2);
+// tree.add();
+
+// tree.add(6);
+// tree.add(5);
+// tree.add(4);
+// tree.add(3);
+// tree.add(2);
 
 tree.display();
+
+// {
+//   "data": 8,
+//   "left": {
+//     "data": 3,
+//     "left": {
+//       "data": 1
+//     },
+//     "right": {
+//       "data": 6,
+//       "left": {
+//         "data": 4
+//       },
+//       "right": {
+//         "data": 7
+//       }
+//     }
+//   },
+//   "right": {
+//     "data": 10,
+//     "right": {
+//       "data": 14,
+//       "left": {
+//         "data": 13
+//       }
+//     }
+//   }
+// }
