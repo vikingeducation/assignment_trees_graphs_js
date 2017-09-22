bcab060658774a2d5917a35a653d221d77cde348
Will Timpson <will.timpson@gmail.com>
2017-09-22 12:36:15 -0400

    such matrix. so display

--- edgeList.js
+++ edgeList.js
@@ -1,97 +1,48 @@
+const Person = require("./Person");
 class AdjacencyMatrix {
   constructor(input) {
     this.matrix = null;
-    if (typeof input === 'number') {
+    if (typeof input === "number") {
       this._buildMatrix(input);
     } else {
       const maxId = input.reduce((acc, edge) => {
         return Math.max(acc, edge[0].id, edge[1].id);
       }, 0);
+      this._buildMatrix(maxId + 1);
 
-      this._buildMatrix(maxId);
+      input.forEach(([left, right, weight]) => {
+        this.add(left.id, right.id, weight);
+      });
     }
   }
 
   _buildMatrix(input) {
-    this.matrix = new Array(input).fill(new Array(input));
+    this.matrix = [...Array(input)].map(() => new Array(input));
   }
 
   add(leftId, rightId, weight) {
     this.matrix[leftId][rightId] = weight;
   }
-}
 
-// Person struct
-class Person {
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
+  display() {
+    this.matrix.forEach(row => console.log(JSON.stringify(row)));
+    // console.log(JSON.stringify(this.matrix, null, 2));
   }
 }
 
-// An edge list of connections between people in the format
-// [ORIGIN_PERSON, DESTINATION_PERSON, WEIGHT]
-// Remember: This is an *undirected* graph.
+const EDGE_LIST = require("./_edgeList");
 
-const EDGE_LIST = [
-  [new Person(1, 'Harry'), new Person(10, 'Alice'), 2],
-  [new Person(1, 'Harry'), new Person(0, 'Bob'), 9],
-  [new Person(1, 'Harry'), new Person(4, 'Michael'), 11],
-  [new Person(1, 'Harry'), new Person(16, 'Peter'), 12],
-  [new Person(2, 'Sally'), new Person(7, 'Dan'), 16],
-  [new Person(2, 'Sally'), new Person(14, 'Mike'), 1],
-  [new Person(2, 'Sally'), new Person(10, 'Alice'), 7],
-  [new Person(3, 'Sam'), new Person(19, 'Boris'), 16],
-  [new Person(4, 'Michael'), new Person(7, 'Dan'), 19],
-  [new Person(4, 'Michael'), new Person(2, 'Sally'), 8],
-  [new Person(4, 'Michael'), new Person(17, 'Andur'), 11],
-  [new Person(4, 'Michael'), new Person(14, 'Mike'), 11],
-  [new Person(5, 'Michelle'), new Person(3, 'Sam'), 9],
-  [new Person(5, 'Michelle'), new Person(10, 'Alice'), 12],
-  [new Person(5, 'Michelle'), new Person(6, 'Alok'), 12],
-  [new Person(7, 'Dan'), new Person(19, 'Boris'), 13],
-  [new Person(7, 'Dan'), new Person(10, 'Alice'), 11],
-  [new Person(7, 'Dan'), new Person(5, 'Michelle'), 1],
-  [new Person(8, 'Nick'), new Person(15, 'Adam'), 1],
-  [new Person(8, 'Nick'), new Person(16, 'Peter'), 20],
-  [new Person(8, 'Nick'), new Person(7, 'Dan'), 9],
-  [new Person(8, 'Nick'), new Person(0, 'Bob'), 9],
-  [new Person(9, 'Olga'), new Person(6, 'Alok'), 11],
-  [new Person(9, 'Olga'), new Person(14, 'Mike'), 20],
-  [new Person(9, 'Olga'), new Person(19, 'Boris'), 12],
-  [new Person(10, 'Alice'), new Person(3, 'Sam'), 19],
-  [new Person(11, 'Donald'), new Person(7, 'Dan'), 19],
-  [new Person(12, 'Garrett'), new Person(10, 'Alice'), 15],
-  [new Person(12, 'Garrett'), new Person(16, 'Peter'), 7],
-  [new Person(12, 'Garrett'), new Person(17, 'Andur'), 16],
-  [new Person(12, 'Garrett'), new Person(18, 'Tom'), 4],
-  [new Person(13, 'Xin'), new Person(8, 'Nick'), 5],
-  [new Person(13, 'Xin'), new Person(5, 'Michelle'), 3],
-  [new Person(13, 'Xin'), new Person(18, 'Tom'), 12],
-  [new Person(13, 'Xin'), new Person(15, 'Adam'), 17],
-  [new Person(14, 'Mike'), new Person(12, 'Garrett'), 10],
-  [new Person(15, 'Adam'), new Person(0, 'Bob'), 18],
-  [new Person(15, 'Adam'), new Person(17, 'Andur'), 8],
-  [new Person(16, 'Peter'), new Person(4, 'Michael'), 12],
-  [new Person(16, 'Peter'), new Person(11, 'Donald'), 10],
-  [new Person(16, 'Peter'), new Person(2, 'Sally'), 1],
-  [new Person(17, 'Andur'), new Person(14, 'Mike'), 9],
-  [new Person(17, 'Andur'), new Person(5, 'Michelle'), 17],
-  [new Person(18, 'Tom'), new Person(14, 'Mike'), 9],
-  [new Person(18, 'Tom'), new Person(16, 'Peter'), 14],
-  [new Person(19, 'Boris'), new Person(2, 'Sally'), 5],
-  [new Person(19, 'Boris'), new Person(18, 'Tom'), 3],
-  [new Person(19, 'Boris'), new Person(12, 'Garrett'), 5],
-  [new Person(19, 'Boris'), new Person(13, 'Xin'), 7]
-];
+const adjacent = new AdjacencyMatrix(EDGE_LIST);
+// adjacent.add(1, 2, 23);
+// adjacent.add(4, 2, 23);
+adjacent.display();
 
-// Helper function to view the edge list
 const printEdgeList = () => {
-  let lines = [' -- Edge List -- '];
+  let lines = [" -- Edge List -- "];
   EDGE_LIST.forEach(e => {
     lines.push(`${e[0].name} <-${e[2]}-> ${e[1].name}`);
   });
-  console.log(lines.join('\n'));
+  console.log(lines.join("\n"));
 };
 
 // Uncomment the fillowing to display your edge list
