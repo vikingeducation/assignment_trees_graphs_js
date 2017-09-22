172dcaadf06b56bfa317b2db42d5f01f084c0a59
EdTriplett <EdTriplett@users.noreply.github.com>
2017-09-22 17:32:24 -0400

    more extra challenges

--- extra_challenge_from_slack.js
+++ extra_challenge_from_slack.js
@@ -25,3 +25,47 @@ Devise a class that when instantiated it creates a tree that maps out all of the
 c          b      c         a     b        a
 
 */
+
+/*
+
+
+Adjacency Matrix/List (Josephus-Like) Problem
+Inspiration:
+https://en.wikipedia.org/wiki/Josephus_problem
+
+Take a group of twenty people with different names, let's use these:
+
+- Aiden
+- Aria
+- Ava
+- Caden
+- Charlotte
+- Elijah
+- Emma
+- Ethan
+- Isabella
+- Jackson
+- Liam
+- Lucas
+- Mason
+- Mia
+- Noah
+- Oliver
+- Olivia
+- Riley
+- Sophia
+- Zoe
+
+Now imagine they are all in a circle. The game will start with a random person.
+
+1) Decide a direction (clockwise/counter-clockwise, forward or reverse) to travel in the circle. Use that direction for all iterations
+2) Decide an ODD number `n` to use as the distance that person will travel in that direction. The number should be 3 or higher to make things interesting. The number cannot be `5`!! Start with `3` to keep things simple, then try `7`... etc...
+3) Select a random person and begin moving that person in the chosen direction around the circle
+4) When the person gets to the nth (selected number `n`) person in that direction they will shake that person's hand
+5) Then that person will start from that position and continue around the circle to the next nth person and do the same
+6) Continue this process until the first person's hand is shook `n` (the chosen number) times
+7) Create both an adjacency matrix and adjacency list to represent the handshakes that took place where the weight is the number of handshakes
+8) Create a `.prettyPrint()` method for both your adjacency list and adjacency matrix that outputs them in a nicely formatted style to view the results
+
+
+*/
