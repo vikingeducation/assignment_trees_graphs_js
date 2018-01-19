88de18c7f410935f19292ad285392b1ec610252b
EdwinYung <eyung7@gmail.com>
2018-01-19 14:05:14 -0500

    almost done with display of tree

--- binaryTree.js
+++ binaryTree.js
@@ -1,14 +1,5 @@
 // BINARY TREE
 
-class BinaryNode {
-  constructor() {
-    this.right = null;
-    this.left = null;
-    this.data = null;
-  }
-}
-
-
 class BinaryTree {
   constructor() {
     this.right = null;
@@ -35,36 +26,65 @@ class BinaryTree {
       this.left.insert(data);
     }
   }
- 
-    printChildrenByLevel(node) {
-      let returnArr = [];
-      this._printLevel(node, 0, returnArr);
-      return returnArr;
+
+  printChildrenByLevel(node) {
+    let returnArr = [];
+    this._printLevel(node, 0, returnArr);
+
+    for (let i = 0; i < returnArr.length; i++) {
+      let spaceBetweenNum = Math.pow(2, returnArr.length - i);
+      let spaceLeftNum = spaceBetweenNum / 2;
+      let spacesBetween = new Array(spaceBetweenNum).join(" ");
+      let spacesLeft = new Array(spaceLeftNum).join(" ");
+      console.log(spacesLeft + returnArr[i] + spacesBetween);
+    }
+    return returnArr;
+  }
+
+  _printLevel(tree, level, returnArr) {
+    let spacesBetweenNum;
+    let spacesBetween = " ";
+    for (let i = 0; i < returnArr.length; i++) {
+      spacesBetweenNum = Math.pow(2, returnArr.length - i);
+      spacesBetween = new Array(spacesBetweenNum).join(" ");
     }
 
-    _printLevel(tree, level, returnArr) {
-      console.log("------------------- START tree.left, tree.right -------------------");
-      console.log(tree.left, tree.right);
-      console.log("-------------------- END tree.left, tree.right --------------------");
-      
-      if (tree.left || tree.right) {
-        if (returnArr[level]) {
-          returnArr[level] += tree.left || "";
-          returnArr[level] += tree.right || "";
-        } else {
-          returnArr[level] = tree.left || "";
-          returnArr[level] = tree.right || "";
-        }
-        let children = [tree.left, tree.right];
-        children.forEach(child => {
-          return this._printLevel(child, level + 1, returnArr);
-        });
+    if (!tree) return "";
+    if (tree.left) {
+      console.log("------------------- START tree.left -------------------");
+      console.log(tree.data + ":");
+      console.log(tree.left);
+      console.log("-------------------- END tree.left --------------------");
+      if (returnArr[level]) {
+        returnArr[level] += spacesBetween + tree.left.data.toString() || "";
+      } else {
+        returnArr[level] = tree.left.data.toString() || "";
       }
-      return "";
     }
 
+    if (tree.right) {
+      console.log("------------------- START tree.right -------------------");
 
-     
+      console.log(tree.data + ":");
+      console.log(tree.right);
+      console.log("-------------------- END tree.right --------------------");
+
+      if (returnArr[level]) {
+        returnArr[level] += spacesBetween + tree.right.data.toString() || "";
+      } else {
+        returnArr[level] = tree.right.data.toString() || "";
+      }
+    }
+
+    if (tree.left || tree.right) {
+      let children = [tree.left, tree.right];
+      children.forEach(child => {
+        return this._printLevel(child, level + 1, returnArr);
+      });
+    }
+
+    return "";
+  }
 }
 
 let binaryTest = new BinaryTree();
@@ -73,6 +93,11 @@ binaryTest.insert(2);
 binaryTest.insert(5);
 binaryTest.insert(1);
 binaryTest.insert(8);
+binaryTest.insert(3);
+binaryTest.insert(7);
+binaryTest.insert(1);
+binaryTest.insert(2);
+binaryTest.insert(3);
+binaryTest.insert(4);
 console.log(JSON.stringify(binaryTest, null, 2));
 console.log(binaryTest.printChildrenByLevel(binaryTest));
-
