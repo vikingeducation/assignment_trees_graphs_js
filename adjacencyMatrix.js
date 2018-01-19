7d7c86e564ece973970050bda1e664478dd956c5
Hai Nguyen <memyselfandhai@gmail.com>
2018-01-19 12:40:07 -0600

    changed nil to x

--- adjacencyMatrix.js
+++ adjacencyMatrix.js
@@ -1,48 +1,53 @@
-let edgeList = require('./edgeList.js');
+let edgeList = require("./edgeList.js");
 
 class adjMatrix {
-  constructor(list){
+  constructor(list) {
     this.matrix = [];
     list.map(edge => {
       this.matrix[edge[0].id] = this.matrix[edge[0].id] || [];
-      this.matrix[edge[0].id][edge[1].id] = edge[2]
+      this.matrix[edge[0].id][edge[1].id] = edge[2];
     });
-    for(let i = 0; i < this.matrix.length; i++){
+    for (let i = 0; i < this.matrix.length; i++) {
       this.matrix[i] = this.matrix[i] || [];
-      for(let j = 0; j < this.matrix.length; j++){
-        if(typeof this.matrix[i][j] === 'undefined'){
-          this.matrix[i][j] = 'nil'
+      for (let j = 0; j < this.matrix.length; j++) {
+        if (typeof this.matrix[i][j] === "undefined") {
+          this.matrix[i][j] = " x ";
         } else {
-          if(this.matrix[i][j].toString().length === 1){
-            this.matrix[i][j] = `  ${this.matrix[i][j]}`
+          if (this.matrix[i][j].toString().length === 1) {
+            this.matrix[i][j] = ` ${this.matrix[i][j]} `;
           }
-          if(this.matrix[i][j].toString().length === 2){
-            this.matrix[i][j] = ` ${this.matrix[i][j]}`
+          if (this.matrix[i][j].toString().length === 2) {
+            this.matrix[i][j] = ` ${this.matrix[i][j]}`;
           }
         }
       }
     }
   }
-  inspect(){
-    let result = ' ';
-    for(let i = 0; i < this.matrix.length; i++){
-      if(i.toString().length === 1){
-        result += `   ${i}`
-      } else if(i.toString().length === 2){
-        result += `  ${i}`
+
+  edgeWeight(row, column) {
+    return Number(this.matrix[row][column]);
+  }
+
+  inspect() {
+    let result = " ";
+    for (let i = 0; i < this.matrix.length; i++) {
+      if (i.toString().length === 1) {
+        result += `   ${i}`;
+      } else if (i.toString().length === 2) {
+        result += `  ${i}`;
       } else {
-        result += ` ${i}`
+        result += ` ${i}`;
       }
     }
     this.matrix.forEach((row, index) => {
       let indexString;
-      if(index.toString().length === 1){
-        indexString = ` ${index}`
+      if (index.toString().length === 1) {
+        indexString = ` ${index}`;
       } else {
-        indexString = `${index}`
+        indexString = `${index}`;
       }
-      result += `\n${indexString}[` + row.toString() + ']'
-    })
+      result += `\n${indexString}[` + row.toString() + "]";
+    });
     return result;
   }
 }
@@ -51,3 +56,4 @@ const testMatrix = new adjMatrix(edgeList);
 
 console.log(testMatrix.inspect());
 
+console.log(testMatrix.edgeWeight(1, 0));
