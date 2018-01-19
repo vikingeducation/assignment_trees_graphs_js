86e1d17c1174fd253f6a0ffd08614ce31554756b
GeneTinderholm <tinderholmgene@gmail.com>
2018-01-19 12:24:53 -0600

    format

--- adjacencyMatrix.js
+++ adjacencyMatrix.js
@@ -7,20 +7,47 @@ class adjMatrix {
       this.matrix[edge[0].id] = this.matrix[edge[0].id] || [];
       this.matrix[edge[0].id][edge[1].id] = edge[2]
     });
-    this.matrix.forEach((row, index) => {
-      row.forEach((cell, cellNum) => {
-        if(typeof this.matrix[index][cellNum] === 'undefined'){
-          this.matrix[index][cellNum] = 'nil'
+    for(let i = 0; i < this.matrix.length; i++){
+      this.matrix[i] = this.matrix[i] || [];
+      for(let j = 0; j < this.matrix.length; j++){
+        if(typeof this.matrix[i][j] === 'undefined'){
+          this.matrix[i][j] = 'nil'
+        } else {
+          if(this.matrix[i][j].toString().length === 1){
+            this.matrix[i][j] = `  ${this.matrix[i][j]}`
+          }
+          if(this.matrix[i][j].toString().length === 2){
+            this.matrix[i][j] = ` ${this.matrix[i][j]}`
+          }
         }
-      })
+      }
+    }
+  }
+  inspect(){
+    let result = ' ';
+    for(let i = 0; i < this.matrix.length; i++){
+      if(i.toString().length === 1){
+        result += `   ${i}`
+      } else if(i.toString().length === 2){
+        result += `  ${i}`
+      } else {
+        result += ` ${i}`
+      }
+    }
+    this.matrix.forEach((row, index) => {
+      let indexString;
+      if(index.toString().length === 1){
+        indexString = ` ${index}`
+      } else {
+        indexString = `${index}`
+      }
+      result += `\n${indexString}[` + row.toString() + ']'
     })
+    return result;
   }
 }
 
 const testMatrix = new adjMatrix(edgeList);
 
-// console.log(testMatrix);
+console.log(testMatrix.inspect());
 
-let print = JSON.stringify(testMatrix.matrix, function (key, value) {return (value === undefined) ? "nil" : value});
-
-console.log(JSON.parse(print))
